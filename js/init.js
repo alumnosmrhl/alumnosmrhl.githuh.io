3fbe0d38a841f38be94433c3911e707a165a051f
alumnosmrhl <118540578+alumnosmrhl@users.noreply.github.com>
2022-11-18 07:53:46 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -8,11 +8,11 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyAkChAmhUzYWjCKR4hWbuqzIVpZqQ5RdNQ",
+  authDomain: "alumnosmrhl43.firebaseapp.com",
+  projectId: "alumnosmrhl43",
+  storageBucket: "alumnosmrhl43.appspot.com",
+  messagingSenderId: "999439671088",
+  appId: "1:999439671088:web:8357d86835eda5e8e3f2f5",
+  measurementId: "G-RG6QHJMWSB"
 });
